b2cf9288483494c12058af87a0ff008b5d9265d8
casalgado <casalgado86@gmail.com>
2020-06-06 09:13:56 -0500

    second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p>{{ places }}</p>
+    <div v-for="city in cities" :key="city.id">
+      <input v-model="city" />
+    </div>
+    <button @click="findCoordenates">find</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import axios from "axios";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      destinations: [
+        {
+          id: 0,
+          active: true,
+          name: "",
+          arrival: "",
+          departure: "",
+          duration: 0,
+        },
+      ],
+      places: [],
+    };
+  },
+  methods: {
+    findCoordenates: function() {
+      axios({
+        method: "GET",
+        url: "https://opentripmap-places-v1.p.rapidapi.com/en/places/geoname",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-rapidapi-host": "opentripmap-places-v1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "a4155f4872msh83b76cd21659288p1f840fjsnae620982cab8",
+          useQueryString: true,
+        },
+        params: {
+          name: this.city,
+        },
+      })
+        .then((response) => {
+          let r = response.data;
+          let obj = {
+            name: r.name,
+            lat: r.lat,
+            lon: r.lon,
+            country: r.country,
+          };
+          this.places.push(obj);
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
